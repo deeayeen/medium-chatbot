332f247c1e7dab9d2603323249bc35a5cf5c62d9
Dan Halperin <dee.aye.en@gmail.com>
2020-05-03 02:22:09 +0300

    add chatbot logic

--- src/App.js
+++ src/App.js
@@ -38,9 +38,62 @@ function App() {
                   {
                     value: 1,
                     label: "Yes! Do you have any more React UX articles?",
+                    trigger: "4",
                   },
-                  { value: 2, label: "No, I have some feedback to leave you" },
+                  {
+                    value: 2,
+                    label: "No, I have some feedback to leave you",
+                    trigger: "5",
+                  },
                 ],
+              },
+              {
+                id: "4",
+                message: "Here are four more ways to upgrade your React UX",
+                trigger: "8",
+              },
+              {
+                id: "5",
+                user: true,
+                trigger: "6",
+              },
+              {
+                id: "6",
+                message:
+                  "Thanks for the feedback, please leave your email and I'll get back to you on that.",
+                trigger: "7",
+              },
+              {
+                id: "7",
+                user: true,
+                end: true,
+              },
+              {
+                id: "8",
+                component: (
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-around",
+                    }}
+                  >
+                    <img
+                      style={{ width: "60px" }}
+                      src={
+                        "https://miro.medium.com/max/1400/1*mKOaaI5WgUCsbQIu3VlooA.gif"
+                      }
+                      alt="gif"
+                    />
+                    <a
+                      style={{ color: "black" }}
+                      href="https://levelup.gitconnected.com/master-ux-with-react-in-2020-four-ways-to-upgrade-your-react-app-user-experience-a053b3ffbf59"
+                    >
+                      Master UX with React in 2020
+                    </a>
+                  </div>
+                ),
+                end: true,
               },
             ]}
             botAvatar="https://miro.medium.com/fit/c/128/128/2*SF7HEfkPH70P7E6VTYVmeg.png"
